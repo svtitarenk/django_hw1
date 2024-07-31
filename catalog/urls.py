de9a423683f8be3b9f71b14e3c5ad817cb53c20e
Stanislav <stanislav.titarenko@gmail.com>
2024-08-01 02:14:43 +0300

    Выполнения заданий по ДЗ 20.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import home, contacts, product_list, product_detail
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
     path('home/', home, name='home'),
+    path('catalog/', product_list, name='product_list'),
+    path('catalog/<int:pk>/', product_detail, name='product_detail'),
     path('contacts/', contacts, name='contacts'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from catalog.models import Product
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def product_list(request):
+    products = Product.objects.all()  # берем все объекты продуктов
+    context = {"products": products}
+    return render(request, 'product_list.html', context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'product_detail.html', context)
 
 
 def contacts(request):
